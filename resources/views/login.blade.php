057b9c1e50981e0862a18bf0dfb50b5c0a9258b4
wesley_nagashima <wesley.nagashima98@gmail.com>
2024-11-21 19:59:39 -0300

    Validação do formulário de Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main_layout')
 @section('content')
+
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6 col-sm-8">
