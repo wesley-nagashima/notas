14478e823f98b9860470183da4d7fc305d61cee2
wesley_nagashima <wesley.nagashima98@gmail.com>
2024-11-21 20:35:11 -0300

    Criação das MIgrations

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PDOException;
 
 class AuthController extends Controller
 {
@@ -13,9 +15,18 @@ class AuthController extends Controller
     public function loginSubmit(Request $request) {
         //Form Validation
         $request->validate(
+            //rules 
             [
-                'text_username' => 'required',
-                'text_password' => 'required'
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            //error messages
+            [
+                'text_username.required'    => 'O Username é obrigatório!',
+                'text_username.email'       => 'O Username deve ser um e-mail válido!',
+                'text_password.required'    => 'O Password é obrigatório!',
+                'text_password.min'         => 'O Password deve ter pelo menos :min caracteres!',
+                'text_password.max'         => 'O Password deve ter no máximo :max caracteres!',
             ]
         );
 
@@ -23,7 +34,15 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK';
+        //test database connection
+        try {
+            DB::connection()->getPdo();
+            echo "Connection is OK!";
+        }
+        catch (PDOException $e) {
+            echo "Connection failed: ". $e->getMessage();
+        }
+
     }
 
     public function logout() {
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.main_layout')
 @section('content')
-
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6 col-sm-8">
